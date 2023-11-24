f78ecbdbe62645fa757660c1e5b57d2e2fdffd9d
michaelee0407 <michaelee0407@gmail.com>
2023-11-24 10:50:13 +0800

    create class Student

--- STUST Pyhton 2023-11-16/python_exercise.py
+++ STUST Pyhton 2023-11-16/python_exercise.py
@@ -1,11 +1,27 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+class Course:
+  def __init__(self, course_name, 
+               course_id, course_credits):
+    self.course_name = course_name
+    self.course_id = course_id
+    self.course_credits = course_credits
+
+    def __str__(self):
+      return f"Course Name: {self.course_name}\nCourse ID = {self.course_id}\nCourse credits = {self.course_credits}"
+
+class Student:
+  def __init__(self, stu_name, stu_id):
+    self.stu_name = stu_name
+    self.stu_id = stu_id
 
   def __str__(self):
-    return f"Name: {self.name}\nAge = {self.age}"    
+    return f"Name: {self.stu_name}\nID = {self.stu_id}"
+  
+  def addCourse(self, object):
+    self.course = object
 
-p1 = Person("John", 36)
-
-print(p1)
+c1 = Course(course_name="套裝軟體應用", 
+            course_id="(G0D17M01",
+            course_credits=3)
+s1 = Student(stu_name="李勇材", stu_id="4a9g0901")
+s1.addCourse(c1)
+print(s1)
